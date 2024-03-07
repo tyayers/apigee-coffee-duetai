d521bfceb02144b0cde0319cfeda1fbeacc9de6f
tyayers <tyler.ayers@gmail.com>
2024-03-07 19:07:57 +0100

    updated test results

--- apigee-ws/scripts/run_tests.sh
+++ apigee-ws/scripts/run_tests.sh
@@ -1,2 +1,2 @@
-hurl --test ../tests/test1_local.hurl --report-html reports/unit_httpbin_local.html
-hurl --test ../tests/test1_prod.hurl --report-html reports/unit_httpbin_prod.html
+hurl --test ../tests/test1_local.hurl --continue-on-error --report-html reports/unit_httpbin_local.html
+# hurl --test ../tests/test1_prod.hurl --report-html reports/unit_httpbin_prod.html
