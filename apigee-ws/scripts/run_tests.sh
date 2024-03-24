65df152c88048d8ae2f15e88c178512609cce1d2
tyayers <tyler.ayers@gmail.com>
2024-03-24 12:29:31 +0100

    updated proxy

--- apigee-ws/scripts/run_tests.sh
+++ apigee-ws/scripts/run_tests.sh
@@ -1,2 +1,2 @@
-hurl --test ../tests/test1_local.hurl --continue-on-error --report-html reports/unit_httpbin_local.html
+hurl --test ../tests/test1_local.hurl --continue-on-error --report-html reports/unit_tests_local --report-junit reports/unit_tests_local
 # hurl --test ../tests/test1_prod.hurl --report-html reports/unit_httpbin_prod.html
